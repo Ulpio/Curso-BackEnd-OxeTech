1f12bf2e6c9e4488f7c9b32b2b6f805a739d519b
Úlpio Netto <ulpionetto0@gmail.com>
2024-02-28 18:15:40 -0300

    add autores

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json())
 
+app.get("/",(req,res) =>{
+    res.status(200).send("Curso do OxeTech")
+})
+
 const livros = [
     {
         id: 1,
@@ -14,10 +18,6 @@ const livros = [
     }
 ]
 
-app.get("/",(req,res) =>{
-    res.status(200).send("Curso do OxeTech")
-})
-
 app.get("/livros",(req,res) =>{
     res.status(200).json(livros)
 })
@@ -25,6 +25,22 @@ app.get("/livros",(req,res) =>{
 app.post("/livros",(req,res) =>{
     livros.push(req.body)
     res.status(201).send("Livro foi criado com sucesso")
+})
+
+const autores = [
+    {
+        id: 1,
+        nome: "J.R.R. Tolkien"
+    }
+]
+
+app.get("/autores",(req,res) =>{
+    res.status(200).json(autores)
+})
+
+app.post("/autores",(req,res) =>{
+    autores.push(req.body)
+    res.status(201).send("Autor foi criado com sucesso")
 })
 
 app.listen(3000,() =>{
